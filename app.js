63a8d838e5371d08d8e0d26c37ec7995e7cb3a58
JeffAbney <jeffmabney@gmail.com>
2019-09-23 10:22:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+  request(
+    { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      console.log("here's body", body)
+      res.send(body);
+    }
+  )
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
