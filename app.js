cf52b2c5d5c15615dca9069b6ce68f45170955a2
JeffAbney <jeffmabney@gmail.com>
2019-09-23 10:59:05 -0500

    add cors header in middleware

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
 const express = require('express');
 const request = require('request');
-var cors = require('cors')
 
 const app = express();
 
 
-app.options('*', cors());
-app.use(cors());
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
-
-app.head("/", cors(), (req, res) => {
-    console.info("HEAD /simple-cors");
-    res.sendStatus(204);
-  });
-app.get('/', cors(), (req, res) => {
-  request(
-    { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
-    (error, response, body) => {
-      if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
-      }
-      console.log("here's body", body)
-      res.send(body);
-    }
-  )
+app.get('/', (req, res) => {
+    request(
+        { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
+        (error, response, body) => {
+            if (error || response.statusCode !== 200) {
+                return res.status(500).json({ type: 'error', message: err.message });
+            }
+            console.log("here's body", body)
+            res.send(body);
+        }
+    )
 });
 
 const PORT = process.env.PORT || 3000;
