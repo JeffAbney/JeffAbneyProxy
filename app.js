3a009a3bcc029907f15a750da0790fbca4e253f3
JeffAbney <jeffmabney@gmail.com>
2019-09-23 10:30:24 -0500

    allow cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const request = require('request');
+var cors = require('cors'); 
 
 const app = express();
+app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
