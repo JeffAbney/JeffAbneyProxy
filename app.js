cdb94a8467966e67926e12ae36229075260dd172
JeffAbney <jeffmabney@gmail.com>
2019-09-24 09:06:05 -0500

    debug from simple

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const express = require('express');
-const request = require('request');
-var bodyParser = require('body-parser');
+//const request = require('request');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-    next();
-});
-
-app.post('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send("OK!")
     // request(
     //     { url: 'https://quotesondesign.com/wp-json/wp/v2/posts?filter[orderby]=rand' },
